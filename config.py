63a91bd04ae794cb89e87a9cd7cb07535efdd554
mestarnour <nourelhouda.mestar@inttic.dz>
2025-08-10 21:56:48 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     NEO4J_URI = "bolt://localhost:7687"  # Adresse de Neo4j
     NEO4J_USER = "neo4j"                # Nom d'utilisateur par défaut
-    NEO4J_PASSWORD = "douda2002"        # Changez par votre mot de passe
+    NEO4J_PASSWORD = ""        # Changez par votre mot de passe
